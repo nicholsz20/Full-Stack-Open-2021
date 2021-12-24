0a4a91f68614fcf814765f5317683f507cc69c9b
nicholsz20 <79768423+nicholsz20@users.noreply.github.com>
2021-12-24 13:55:47 -0500

    Update App.js

--- Part1-1d exercise/App.js
+++ Part1-1d exercise/App.js
@@ -1,5 +1,35 @@
 import React, { useState } from 'react'
 
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return(
+      <div>
+        <p>
+          No feedback given
+        </p>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <p>
+          {props.text} {props.counter}
+          <br />
+          {props.text1} {props.counter1}
+          <br />
+          {props.text2} {props.counter2}
+          <br />
+          {props.text3} {props.total}
+          <br />
+          {props.text4} {props.avg}
+          <br />
+          {props.text5} {props.positive}%
+      </p>
+    </div>
+    )
+  }
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -19,36 +49,8 @@ const Feedback= (props) => {
   <h1>{props.name}</h1>
   )
 }
-const Pos= (props) => {
-  return(
-    <p>
-      {props.text} {props.positive}%
-    </p>
-  )
-}
-const Avg= (props) => {
-  return(
-    <p>
-      {props.text} {props.avg}
-    </p>
-  )
-}
-const All = (props) => {
-  return(
-    <p>
-      {props.text} {props.total}
-    </p>
-  )
-}
-const Statistics= (props) => {
-  return(
-    <div>
-    <p>
-      {props.text} {props.counter}
-    </p>
-  </div>
-  )
-}
+
+
 const Button= (props) => {
   return(
     <div style={{ display: "inline-block"}}>
@@ -70,24 +72,20 @@ const Button= (props) => {
         <Statistics 
         text={"good"}
         counter={good}
-        />
-        <Statistics
-        text={"neutral"}
-        counter={neutral}
-        />
-        <Statistics
-        text={"bad"}
-        counter={bad}
-        />
-        <All
-        text={"all"}
-        total={total}/>
-        <Avg 
-        text={"average"}
+
+        text1={"neutral"}
+        counter1={neutral}
+
+        text2={"bad"}
+        counter2={bad}
+  
+        text3={"all"}
+        total={total}
+
+        text4={"average"}
         avg={avg}
-        />
-        <Pos
-        text={"positive"}
+
+        text5={"positive"}
         positive={positive} />
     </div>
   )
